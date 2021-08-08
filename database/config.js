a68a03ea60c0e43bb4c1226acb1e1eb0429ca28b
José Monar <jl_mj_17@hotmail.com>
2021-08-08 02:12:32 -0500

    Connecting Node with Mogo Atlas using mongoose librarie.

---
+++ database/config.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+
+        console.log("DB Online...");
+    } catch (error) {
+        console.log(error);
+        throw new Error("Error during database connection.");
+    }
+};
+
+module.exports = {
+    dbConnection,
+};
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const { dbConnection } = require("./database/config");
 require("dotenv").config();
 
 // Creating express server
 const app = express();
+
+// Database connection
+dbConnection();
 
 // Public sources
 app.use(express.static("public"));
