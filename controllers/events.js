1865eb0ad30852a8c0b5b6f4de36005e2abda62c
José Monar <jl_mj_17@hotmail.com>
2021-08-09 03:15:26 -0500

    Storing events on database and configuring how Event Schema will serialize.

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 const getEvents = (req, res = response) => {
     try {
@@ -16,13 +17,30 @@ const getEvents = (req, res = response) => {
     }
 };
 
-const createEvent = (req, res = response) => {
+const createEvent = async (req, res = response) => {
     console.log(req.body);
+    console.log(req.uid);
 
-    return res.json({
-        ok: true,
-        msg: "Create Event",
-    });
+    try {
+        const event = new Event(req.body);
+        // We have uid and name of user because we save it in the request when we check token
+        // in jwtValidator middleware
+        event.user = req.uid;
+
+        const eventSaved = await event.save();
+
+        return res.json({
+            ok: true,
+            event: eventSaved,
+        });
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            ok: false,
+            msg: "Unexpeted error, contact to support.",
+        });
+    }
 };
 
 const updateEvent = (req, res = response) => {
--- models/Event.js
+++ models/Event.js
@@ -23,4 +23,16 @@ const EventSchema = Schema({
     },
 });
 
+// Here we specify how we want the model schema will serialize, we can specify other configurations here too.
+EventSchema.method("toJSON", function () {
+    // with "this" we are referencing current object that we are serializing
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+
+    // We only changes how "_id" field will be showed in some response of any request,
+    // and we discard "__v". It wont change atributes names on database, there will keep
+    // the names "_id" and "__v"
+    return object;
+});
+
 module.exports = model("Event", EventSchema);
